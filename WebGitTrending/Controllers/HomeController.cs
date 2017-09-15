309a45fc48292c83b3da11f0255db06d03f802b3
lczavaski <lczavaski@COPENGNPE00RBYG>
2017-09-15 17:28:05 -0300

    Alteração entity

--- WebGitTrending/Controllers/HomeController.cs
+++ WebGitTrending/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Web.Mvc;
+using static WebGitTrending.Models.TrendingModel;
 
 namespace WebGitTrending.Controllers
 {
@@ -25,6 +27,39 @@ namespace WebGitTrending.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ReadGit()
+        {
+            var currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+            var lastDate = DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd");
+
+            var url = "https://api.github.com/search/repositories?q=pushed:" + lastDate + ".." + currentDate + "&sort=stars&order=desc";
+
+            HttpWebRequest webRequest = System.Net.WebRequest.Create(url) as HttpWebRequest;
+            if (webRequest != null)
+            {
+                webRequest.Method = "GET";
+                webRequest.UserAgent = "Find-GitHubRepository-App";
+                webRequest.Accept = "application/vnd.github.v3+json";
+                webRequest.ServicePoint.Expect100Continue = false;
+
+                try
+                {
+                    using (StreamReader responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream(), Encoding.UTF8))
+                    {
+                        string reader = responseReader.ReadToEnd();
+                        RootObject obj = JsonConvert.DeserializeObject<RootObject>(reader);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Message = "Não foi possível efetuar a leitura da API.";
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
